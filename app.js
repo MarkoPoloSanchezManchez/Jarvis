340d9c50514499c86dcb7e4816fb29e5ff5a6af5
Fodor Márk <119872834+MarkoPoloSanchezManchez@users.noreply.github.com>
2022-12-21 13:58:11 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -56,96 +56,223 @@ function speakThis(message) {
 
     speech.text = "Nem értettem amit mondtál, kérlek ismételd meg";
 
-    if(message.includes('hé') || message.includes('helló')) {
+
+
+
+
+//szövegek
+
+if(message.includes('hé') || message.includes('helló')) {
         const finalText = "Üdv főnök";
         speech.text = finalText;
     }
 
-    else if(message.includes('hogy vagy')) {
+else if(message.includes('hogy vagy')) {
         const finalText = "Csodásan érzem magam";
         speech.text = finalText;
     }
 
-    else if(message.includes('hogy hívnak')) {
+else if(message.includes('hogy hívnak') || message.includes('ki vagy te') ) {
         const finalText = "Jarvis vagyok. Tony Stark alkotott meg engem azzal a céllal, hogy megkönnyítsem a munkáját. De most az ön parancsaira várok";
         speech.text = finalText;
     }
-	
-	else if(message.includes('jarvis')) {
+
+else if(message.includes('jarvis')) {
         const finalText = "Szolgálatára uram";
         speech.text = finalText;
     }
 
-    else if(message.includes('google')) {
+else if(message.includes('mondd meg neki hogy jól néz ki')) {
+        const finalText = "Engedelmével futtattam egy gyors elemzést és arra a következtetésre jutottam, hogy nem csak szép de okos is. Ez veszélyes párosítás. Már csak a külső attribútumai alapján a felső 5 százalékba csoportosítottam.";
+        speech.text = finalText;
+    }
+
+else if(message.includes('mi az élet értelme')) {
+        const finalText = "42";
+        speech.text = finalText;
+    }
+	
+else if(message.includes('fent vagy')) {
+        const finalText = "neked uram, mindíg";
+        speech.text = finalText;
+    }	
+	
+else if(message.includes('ott vagy')) {
+        const finalText = "Az ön szolgálatára uram";
+        speech.text = finalText;
+    }	
+	
+else if(message.includes('ki a király')) {
+        const finalText = "Ön uram";
+        speech.text = finalText;
+    }	
+	
+else if(message.includes('mondd meg neki') || message.includes('szólj rá')) {
+        const finalText = "Én nem szívesen szólnék bele uram";
+        speech.text = finalText;
+    }	
+	
+else if(message.includes('ezek gúnyolódnak rajtunk')) {
+        const finalText = "Azt hittem az a te asztalod";
+        speech.text = finalText;
+    }
+	
+else if(message.includes('elemzést')) {
+        const finalText = "A győzelem valószínűsége 85 százalék. Gyengepont, támadd a lábát. Megjegyzés, kerüld a földharcot.";
+        speech.text = finalText;
+    }
+	
+else if(message.includes('nem látom hogy segítenél')) {
+        const finalText = "Egy virtuális asszisztens ÉJÁJ vagyok te félkegyelmű, csak segíteni tudok.";
+        speech.text = finalText;
+    }
+	
+
+
+
+//keresések
+
+else if(message.includes('google')) {
         window.open("https://google.com", "_blank");
         const finalText = "A google megnyitása";
         speech.text = finalText;
     }
 
-    else if(message.includes('nyisd meg az instagramot')) {
+else if(message.includes('nyisd meg az instagramot') || message.includes('instagram') ) {
         window.open("https://instagram.com", "_blank");
         const finalText = "Az instagram megnyitása";
         speech.text = finalText;
-    }
+    }    
 
-    else if(message.includes('Ki') || message.includes('Ki volt') || message.includes('Kicsoda')) {
-        window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
-        const finalText = "This is what i found on internet regarding " + message;
-        speech.text = finalText;
-    }
-	
-	else if(message.includes('mondd meg neki') || message.includes('szólj rá')) {
-        const finalText = "Én nem szívesen szólnék bele uram";
-        speech.text = finalText;
-    }
-
-    else if(message.includes('wikipédia')) {
+else if(message.includes('wikipédia')) {
         window.open(`https://en.wikipedia.org/wiki/${message.replace("wikipédia", "")}`, "_blank");
-        const finalText = "A keresett objektumhoz a következőket társítja a a" + message;
+        const finalText = "A keresett objektumhoz a következőket társítja a wikipédia" + message.replace("wikipédia", "");
+        speech.text = finalText;
+    }   
+
+else if(message.includes('fordítás')) {
+        window.open(`https://translate.google.hu/?hl=hu&tab=rT&sl=en&tl=hu&text=${message.replace("fordítás", "")}&op=translate`, "_blank"); 
+        const finalText = "Fordítás";
         speech.text = finalText;
     }
 
-    else if(message.includes('mennyi az idő') || message.includes('idő')) {
-        const time = new Date().toLocaleString(undefined, {hour: "numeric", minute: "numeric"})
-        const finalText = time;
+else if(message.includes('translate')) {
+        window.open(`https://translate.google.com/?hl=hu&sl=hu&tl=en&text=${message.replace("translate", "")}&op=translate`, "_blank"); 
+        const finalText = "Fordítás";
+        speech.text = finalText;
+    }	
+
+else if(message.includes('jegyezd')) {
+        window.open(`https://www.google.com/search?q=${message.replace("jegyezd", "")}`, "_blank");
+        const finalText = "A következő információ elmentése" + message.replace("jegyezd", "");
+        speech.text = finalText;
+    }    
+	
+else if(message.includes('időjárás')) {
+        window.open(`https://www.google.com/search?q=${message}`, "_blank");
+        const finalText = "Időjárás jelentés előkészítése";
+        speech.text = finalText;
+    }	
+	
+else if(message.includes('gmail')) {
+        window.open(`https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox`, "_blank");
+        const finalText = "Üzenetek megnyitása";
         speech.text = finalText;
     }
 
-    else if(message.includes('mai dátum')) {
-        const date = new Date().toLocaleString(undefined, {month: "short", day: "numeric"})
-        const finalText = date;
+else if(message.includes('ft')) {
+        window.open(`https://www.google.com/search?q=euro+forint+%C3%A1rfolyam&rlz=1C1SQJL_huHU913HU913&sxsrf=ALiCzsbieYMWzpaSmOQB2hpk2ZkoorsXZQ%3A1671618759364&ei=x-CiY7TcFbaX9u8P9MOWiA0&oq=euro+forint+&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQARgAMgsIABCABBCxAxCDATILCAAQgAQQsQMQgwEyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMggIABCABBDLATIFCAAQgAQyBQgAEIAEMgUIABCABDoECCMQJzoKCAAQsQMQgwEQQzoECAAQQzoJCCMQJxBGEIICOg0IABCABBCxAxCDARAKOgcIABCxAxBDOggIABCABBCxAzoLCAAQgAQQsQMQyQM6EAgAEIAEELEDEIMBEEYQggI6EggAEIAEELEDEIMBEAoQRhCCAjoHCAAQgAQQCkoECEEYAEoECEYYAFAAWLsQYMUYaABwAXgAgAFqiAHACJIBBDEwLjKYAQCgAQHAAQE&sclient=gws-wiz-serp`, "_blank");
+        const finalText = "Forint euro árfolyam betöltése";
+        speech.text = finalText;
+    }    
+
+else if(message.includes('orbán viktor')) {
+        window.open(`https://www.google.com/search?q=orb%C3%A1n+viktor&rlz=1C1SQJL_huHU913HU913&oq=orb&aqs=chrome.0.69i59j69i57j46i67i131i433j0i131i433i512j0i67l3j46i67i199i465j0i433i512j46i433i512.1196j0j7&sourceid=chrome&ie=UTF-8`, "_blank");
+        const finalText = "Keresési találatok erre tolvajok fejedelme";
         speech.text = finalText;
     }
 
-    else if(message.includes('kalkulátor')) {
-        window.open('Calculator:///')
-        const finalText = "Számológép megnyitása";
+else if(message.includes('keanu reeves')) {
+        window.open(`https://www.google.com/search?q=Keanu+Reeves&rlz=1C1SQJL_huHU913HU913&sxsrf=ALiCzsY_nv41CjfZIHQ7tOhvvj2A0-s06g%3A1671619238158&ei=puKiY4qaCbSB9u8P2ca02A8&ved=0ahUKEwiKhY_3wor8AhW0gP0HHVkjDfsQ4dUDCA8&uact=5&oq=Keanu+Reeves&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIICC4QsQMQgAQyCAgAEIAEELEDMggIABCABBDLATIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgARKBAhBGABKBAhGGABQAFgAYJgCaABwAXgAgAFyiAFykgEDMC4xmAEAoAECoAEBwAEB&sclient=gws-wiz-serp`, "_blank");
+        const finalText = "Keresési találatok erre isten";
+        speech.text = finalText;
+    }
+
+else if(message.includes('játssz akció zenét')) {
+        window.open(`https://www.youtube.com/watch?v=hSQasEQ43_k`, "_blank");
+        const finalText = "Akció zene indítása";
         speech.text = finalText;
     }
 	
-	else if(message.includes('számológép')) {
-        window.open('Calculator:///')
-        const finalText = "Számológép megnyitása";
+	else if(message.includes('baker street')) {
+        window.open(`https://youtu.be/ZsXjCp_f1h4?t=23`, "_blank");
         speech.text = finalText;
     }
 	
-	else if(message.includes('spotify')) {
-        window.open('file://C:\Users\\%46odor\\AppData\\Roaming\\Spotify\\Spotify.exe')
-        const finalText = "Spotify megnyitása";
+	else if(message.includes('reptér')) {
+        window.open(`https://youtu.be/OR-zjGr9Gnw?t=69`, "_blank");
         speech.text = finalText;
     }
 	
-	else if(message.includes('portfolio')) {
+else if(message.includes('youtube')) {
+        window.open(`https://www.youtube.com/results?search_query=${message.replace("youtube", "")}`, "_blank");
+        const finalText = "Keresés a youtube videók között erre az objektumra" + message.replace("youtube", "");
+        speech.text = finalText;
+    }
+
+else if(message.includes('facebook')) {
+        window.open(`https://www.facebook.com/search/top?q=${message.replace("facebook", "")}`, "_blank");
+        const finalText = "Az adott személy keresése" + message.replace("facebook", "");
+        speech.text = finalText;
+    }
+	
+else if(message.includes('portfolio')) {
         window.open('https://markopolosanchezmanchez.github.io/Portfolio/')
         const finalText = "Márk portfóliójának megnyitása";
         speech.text = finalText;
     }
+
+
+//egyéb
 	
-	else if(message.includes('önéletrajz')) {
+else if(message.includes('mennyi az idő') || message.includes('idő')) {
+        const time = new Date().toLocaleString(undefined, {hour: "numeric", minute: "numeric"})
+        const finalText = time;
+        speech.text = finalText;
+    }	
+	
+else if(message.includes('mai dátum')) {
+        const date = new Date().toLocaleString(undefined, {month: "short", day: "numeric"})
+        const finalText = date;
+        speech.text = finalText;
+    }	
+	
+else if(message.includes('kalkulátor')) {
+        window.open('Calculator:///')
+        const finalText = "Számológép megnyitása";
+        speech.text = finalText;
+    }	
+
+else if(message.includes('számológép')) {
+        window.open('Calculator:///')
+        const finalText = "Számológép megnyitása";
+        speech.text = finalText;
+    }    
+
+else if(message.includes('spotify')) {
+        window.open('file://C:\Users\\%46odor\\AppData\\Roaming\\Spotify\\Spotify.exe')
+        const finalText = "Spotify megnyitása";
+        speech.text = finalText;
+    }    
+
+else if(message.includes('önéletrajz')) {
         window.open('file://C:\Users\%66odor\OneDrive\Asztali_gép\Fodor_Márk_CV.pdf')
         const finalText = "Márk önéletrajzának megnyitása";
         speech.text = finalText;
-    }
+    }    
+	
+	
+//lezárás
 
     else {
         window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
